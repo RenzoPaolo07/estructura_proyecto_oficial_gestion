c8427818b4a809196f523db9a9798d99c2bd6c68
RenzoPaolo07 <60995421@continental.edu.pe>
2025-06-14 20:36:32 -0500

    Update Proyecto_ABR.cpp
    Creacion de la estructura "Persona", la cual sera la base del arbol

--- Proyecto_ABR.cpp
+++ Proyecto_ABR.cpp
@@ -1,1 +1,12 @@
-//Codigo para las respuestas del informe 
+//Codigo para las respuestas del informe
+//Primero definimos la estructura Persona, que sera la base del arbol:
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Persona {
+    int anioNacimiento;
+    string nombre;
+    Persona* izquierda;
+    Persona* derecha;
+};
